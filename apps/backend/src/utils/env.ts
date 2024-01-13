71a1dab8d807b968fe0a7ed801c43376a7469087
Batin Evirgen <batinevirgen@gmail.com>
2024-01-13 12:55:54 +0300

    feat(backend): add env helper to load environment variables

---
+++ apps/backend/src/utils/env.ts
@@ -0,0 +1,29 @@
+import { log } from '@utils/logger';
+
+// TODO: We should use keyof NodeJS.ProcessEnv for `name` but doesn't work :(
+export function env(name: string, defaultValue?: string): string;
+export function env(name: string, defaultValue?: number): number;
+export function env(name: string, defaultValue?: string | number) {
+  const value = process.env[name];
+  if (!value) {
+    if (defaultValue) return defaultValue;
+
+    log.fatal(`Missing environment variable: ${name}`);
+    process.exit(1);
+  }
+
+  if (typeof defaultValue === 'string') return value;
+
+  const number = tryParseInt(value);
+  if (number) return number;
+
+  log.fatal(`Invalid environment variable ${name}=${value}, expected number`);
+  process.exit(1);
+}
+
+function tryParseInt(value: string) {
+  const parsed = parseInt(value);
+  if (isNaN(parsed)) return null;
+
+  return parsed;
+}
