06b251a1cf14194aadd0cdba3841f8aabf841061
Batin Evirgen <batinevirgen@gmail.com>
2024-01-13 13:00:09 +0300

    feat(backend): add basic server using Hono

---
+++ apps/backend/src/bootstrap.ts
@@ -0,0 +1,40 @@
+import { log } from '@utils/logger';
+import { Hono } from 'hono';
+
+let requestsServed = 0;
+
+export function getServer() {
+  const app = new Hono({
+    strict: false,
+  });
+
+  app.use('*', async (_, next) => {
+    requestsServed++;
+    await next();
+  });
+
+  return app;
+}
+
+export function addShutdownEventListeners() {
+  ['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, () => {
+      log.info(`Received ${signal}, shutting down gracefully...`);
+      gracefulShutdown();
+    });
+  });
+}
+
+function gracefulShutdown() {
+  const runtime = Bun.nanoseconds() * 1.666667 * 1e-11;
+
+  log.info(
+    `API server ran for ${runtime.toFixed(
+      2
+    )} minutes and served ${requestsServed} requests`
+  );
+
+  log.info('See you later!');
+
+  process.exit(0);
+}
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,17 @@
+import { env } from '@utils/env';
 import { log } from '@utils/logger';
+import { addShutdownEventListeners, getServer } from 'bootstrap';
 
-log.info(`Hello from Bun v${Bun.version}`);
+addShutdownEventListeners();
+
+const port = env('PORT', 3000);
+Bun.serve({
+  development: env('NODE_ENV', 'development') === 'development',
+  port,
+  fetch(request, server) {
+    const app = getServer();
+    return app.fetch(request, server);
+  },
+});
+
+log.info(`API server started on localhost:${port}`);
