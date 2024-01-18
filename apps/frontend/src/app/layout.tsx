b5e4754e53d7fdeccbbb0a8ea675ffaff0f4e7bc
Batin Evirgen <batinevirgen@gmail.com>
2024-01-18 17:01:38 +0300

    feat(frontend): add Vercel Sans and Mono font and use it as default

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
+import { cn } from '@utils/tw';
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
 import type { PropsWithChildren } from 'react';
 import './globals.css';
 
 type Props = PropsWithChildren;
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
+    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en">
       <body>{children}</body>
     </html>
   );
--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -2,5 +2,13 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
 };
 export default config;
