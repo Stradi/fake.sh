1050da060b827465cfda571dc5edb92808a97d20
Batin Evirgen <batinevirgen@gmail.com>
2024-01-23 14:49:33 +0300

    feat(frontend): add CreateProjectDialog and use that in /dashboard route

---
+++ apps/frontend/src/app/(dashboard)/dashboard/(general-layout)/_components/create-project-dialog.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import ScalingDialogRoot from '@components/scaling-dialog';
+import { Button } from '@components/ui/button';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@components/ui/dialog';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@components/ui/form';
+import { Input } from '@components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import type { CreateProjectFormType } from '@lib/backend/projects/projects-types';
+import { CreateProjectFormSchema } from '@lib/backend/projects/projects-types';
+import useProjectsApi from '@lib/backend/projects/use-projects-api';
+import { startTransition, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+export default function CreateProjectDialog() {
+  const [open, setOpen] = useState(false);
+
+  const api = useProjectsApi();
+  const form = useForm<CreateProjectFormType>({
+    resolver: zodResolver(CreateProjectFormSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  function onSubmit(data: CreateProjectFormType) {
+    // @ts-expect-error -- react@beta supports async functions in startTransition
+    startTransition(async () => {
+      const response = await api.createProject(data);
+      if (!response.success) {
+        form.setError('root', {
+          message: response.error.message,
+        });
+        toast.error(response.error.message);
+        return;
+      }
+
+      toast.success('Project created successfully');
+      setOpen(false);
+      form.reset();
+    });
+  }
+
+  return (
+    <ScalingDialogRoot
+      onClose={() => {
+        setOpen(false);
+      }}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      open={open}
+    >
+      <DialogTrigger asChild>
+        <Button>Create Project</Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[450px]">
+        <DialogHeader>
+          <DialogTitle>Create Project</DialogTitle>
+          <DialogDescription>
+            Create a new project and generate automatic API routes for it.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-4"
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises -- ...
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Project name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="My New Project Name"
+                      {...field}
+                      autoComplete="off"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {form.formState.errors.root ? (
+              <p className="text-sm text-red-600">
+                {form.formState.errors.root.message}
+              </p>
+            ) : null}
+            <Button type="submit">Create</Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </ScalingDialogRoot>
+  );
+}
--- apps/frontend/src/app/(dashboard)/dashboard/(general-layout)/page.tsx
+++ apps/frontend/src/app/(dashboard)/dashboard/(general-layout)/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <div>Dashboard Page - Select Project</div>;
+import Container from '@components/container';
+import type { ApiProject } from '@lib/backend/backend-types';
+import createServerComponentClient from '@lib/backend/client/create-server-component-client';
+import Header from '../_components/header';
+import CreateProjectDialog from './_components/create-project-dialog';
+
+export default async function Page() {
+  const backendClient = await createServerComponentClient();
+  const projects = await backendClient.sendRequest<{
+    message: string;
+    payload: ApiProject[];
+  }>('/api/v1/projects?own=1&limit=100');
+
+  if (!projects.success) {
+    throw new Error('Failed to fetch projects');
+  }
+
+  return (
+    <div>
+      <Header title="My Projects">
+        <CreateProjectDialog />
+      </Header>
+      <hr />
+      <br />
+      <Container>
+        {projects.data.payload.toReversed().map((project) => (
+          <p key={project.id}>{project.name}</p>
+        ))}
+      </Container>
+    </div>
+  );
 }
