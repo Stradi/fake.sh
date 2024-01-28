0a6fc1cac6fc027c613c649b63bfcffd723ea19f
Batin Evirgen <batinevirgen@gmail.com>
2024-01-28 21:06:03 +0300

    fix(frontend): update LogsTable

--- apps/frontend/src/app/(dashboard)/dashboard/(project-layout)/[projectSlug]/versions/_components/logs-table.tsx
+++ apps/frontend/src/app/(dashboard)/dashboard/(project-layout)/[projectSlug]/versions/_components/logs-table.tsx
@@ -3,6 +3,19 @@
 import { DataTable } from '@components/ui/data-table';
 import type { ApiSchemaLogs } from '@lib/backend/backend-types';
 import type { ColumnDef } from '@tanstack/react-table';
+import { toRelativeDate } from '@utils/date';
+import { cn } from '@utils/tw';
+
+const MethodToColor: Record<
+  'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+  string
+> = {
+  GET: 'bg-green-100 text-green-800',
+  POST: 'bg-blue-100 text-blue-800',
+  PUT: 'bg-yellow-100 text-yellow-800',
+  PATCH: 'bg-yellow-100 text-yellow-800',
+  DELETE: 'bg-red-100 text-red-800',
+};
 
 const columns: ColumnDef<ApiSchemaLogs>[] = [
   {
@@ -11,15 +24,35 @@ const columns: ColumnDef<ApiSchemaLogs>[] = [
   },
   {
     header: 'Timestamp',
-    accessorKey: 'created_at',
+    cell: ({ row }) => {
+      return (
+        <time dateTime={new Date(row.original.created_at).toString()}>
+          {toRelativeDate(new Date(row.original.created_at))}
+        </time>
+      );
+    },
   },
   {
     header: 'URL',
-    accessorKey: 'url',
+    cell: ({ row }) => {
+      const url = new URL(row.original.url);
+      return <p>{url.pathname}</p>;
+    },
   },
   {
     header: 'Method',
-    accessorKey: 'method',
+    cell: ({ row }) => {
+      return (
+        <span
+          className={cn(
+            'select-none rounded-full px-2 py-1 text-center text-xs',
+            MethodToColor[row.original.method]
+          )}
+        >
+          {row.original.method}
+        </span>
+      );
+    },
   },
   {
     header: 'Status',
@@ -28,13 +61,34 @@ const columns: ColumnDef<ApiSchemaLogs>[] = [
   {
     header: 'Body',
     cell: ({ row }) => {
-      return <p>{JSON.stringify(row.original.body)}</p>;
+      return (
+        <pre className="font-mono text-xs">
+          {JSON.stringify(
+            JSON.parse(row.original.body as unknown as string),
+            null,
+            2
+          )}
+        </pre>
+      );
     },
   },
   {
     header: 'Headers',
     cell: ({ row }) => {
-      return <p>{JSON.stringify(row.original.headers)}</p>;
+      const headerStr = row.original.headers as unknown as string;
+      const headers = Object.entries(JSON.parse(headerStr));
+
+      const dom = headers.map(([key, value]) => {
+        const safeValue = value as string;
+
+        return (
+          <p key={key}>
+            <span>{key}</span>: <span>{safeValue}</span>
+          </p>
+        );
+      });
+
+      return <div className="space-y-1">{dom}</div>;
     },
   },
 ];
@@ -45,7 +99,7 @@ type Props = {
 
 export default function LogsTable({ logs }: Props) {
   return (
-    <div className="w-fit">
+    <div className="w-full">
       <DataTable columns={columns} data={logs} />
     </div>
   );
